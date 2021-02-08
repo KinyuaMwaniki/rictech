ab35fe1ffb397be1c67256d5e2476791217d8071
Felix Mwaniki <kinyuafelix4@gmail.com>
2021-02-08 13:38:02 +0300

    Create partial blade files

---
+++ resources/views/adminlte/partials/header.blade.php
@@ -0,0 +1,10 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+   @include('adminlte.partials.css') 
+</head>
